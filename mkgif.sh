a3ced44a2ca33efba543f648ec7787a41efd3cb3
Vilson Farias <vilson.farias@arctouch.com>
2018-07-24 16:43:04 -0300

    - Capture video from the current simulator - Waits until the video has been saved to disk before converting into gif - Remove the temporary files in the end of the convertion

--- mkgif.sh
+++ mkgif.sh
@@ -8,9 +8,16 @@ mkgif() {
 	outputFileName="$(basename "$fileName" .mov).gif"
 	echo "Saving $outputFileName"
 
+	echo Creating a video from the current simulator
+    xcrun simctl io booted recordVideo --type=mp4 $inputFileName
+    wait
+
     echo Converting the video to gif
 	ffmpeg -v warning -i $inputFileName -vf "$filters,palettegen" -y $palette
 	ffmpeg -v warning -i $inputFileName -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $outputFileName
+	
+	echo Removing the temporary file
+	rm -f $inputFileName
 }
 
 for fileName in "$@"
